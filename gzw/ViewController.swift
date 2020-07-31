0a45aa9eeb90205f20a15a6dee62857439a349eb
陈杜彬 <434485356@qq.com>
2020-07-31 10:31:04 +0800

    公开上传

---
+++ gzw/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  zthc
+//
+//  Created by mac on 2020/4/26.
+//  Copyright © 2020 wginfo. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController ,UIScrollViewDelegate{
+
+    @IBOutlet weak var theWebView: WKWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        loadUrl(url: "http://192.168.43.93:4800", web: theWebView)//测试
+        //
+        //loadUrl(url: "http://zhuhai.test.wginfo.com.cn", web: theWebView)// 智慧国资
+        
+     
+        
+    }
+    // 加载请求的方法
+    func loadUrl(url:String,web:WKWebView){
+        // 载入输入的请求
+        let aUrl=NSURL(string:(url))
+        let urlRequest=NSURLRequest(url:aUrl! as URL)
+        web.load(urlRequest as URLRequest)
+    }
+    
+    func viewForZooming(in scrollView:UIScrollView)->UIView?{
+        return nil
+    }
+
+
+}
+
